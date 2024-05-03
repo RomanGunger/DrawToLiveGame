57052b4053f6e9cab08ba60b11c91cd20fca7b08
RomanGunger <71374915+RomanGunger@users.noreply.github.com>
2024-05-03 23:13:13 +0300

    Score

---
+++ Assets/Sources/Scripts/Level/Score.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int CurrentScore { get; private set; } = 0;
+
+    public static Action<int> ScoreAdded;
+
+    private void Start()
+    {
+        CurrencyItem.ScoreItemCollected += OnScoreItemCollected;
+    }
+
+    void OnScoreItemCollected()
+    {
+        CurrentScore++;
+
+        ScoreAdded?.Invoke(CurrentScore);
+    }
+}
