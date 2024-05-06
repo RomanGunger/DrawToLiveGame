038886d845419e35b137179ece4da5839ad0428e
RomanGunger <71374915+RomanGunger@users.noreply.github.com>
2024-05-06 16:30:47 +0300

    FIx animations system

--- Assets/Sources/Scripts/Unit/RobotsAnimationsHandler.cs
+++ Assets/Sources/Scripts/Unit/RobotsAnimationsHandler.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class RobotsAnimationsHandler : UnitAnimationsHandler
 {
+    Moving moving;
+
     protected override void Start()
     {
         base.Start();
+
+        moving = GetComponentInParent<Moving>();
+
+        var rnd = new System.Random();
 
         animator.SetInteger("RandomIdle", rnd.Next(0, animator.GetInteger("AnimationsCount")));
         animator.SetInteger("RandomRun", rnd.Next(0, animator.GetInteger("AnimationsCount")));
--- Assets/Sources/Scripts/Unit/UnitAnimationsHandler.cs
+++ Assets/Sources/Scripts/Unit/UnitAnimationsHandler.cs
@@ -2,30 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitAnimationsHandler : MonoBehaviour
+public abstract class UnitAnimationsHandler : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    Animator animator;
-    Moving moving;
-=======
-    [SerializeField] protected Animator animator;
-    [SerializeField] protected Moving moving;
->>>>>>> Stashed changes
+    protected Animator animator;
 
-    private void Start()
+    protected virtual void Start()
     {
         animator = GetComponent<Animator>();
-        moving = GetComponentInParent<Moving>();
 
         var rnd = new System.Random();
 
-        animator.SetInteger("RandomIdle", rnd.Next(0, animator.GetInteger("AnimationsCount")));
-        animator.SetInteger("RandomRun", rnd.Next(0, animator.GetInteger("AnimationsCount")));
         animator.SetFloat("CycleOffset", (float)rnd.NextDouble());
-    }
-
-    private void Update()
-    {
-        animator.SetFloat("Speed", moving.Speed);
     }
 }
