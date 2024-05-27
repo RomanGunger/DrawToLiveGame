b917338671310b27096a974e49adaca185abfeaf
RomanGunger <71374915+RomanGunger@users.noreply.github.com>
2024-05-27 13:47:51 +0300

    апдейт меню панели
    добавил затемнение при открытии окон

--- Assets/Sources/Scripts/UI/MainMenu/MenuPanel.cs
+++ Assets/Sources/Scripts/UI/MainMenu/MenuPanel.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class MenuPanel : MonoBehaviour
 {
@@ -18,12 +17,14 @@ public class MenuPanel : MonoBehaviour
     public virtual async Task Open(float durration)
     {
         uiBlocker.gameObject.SetActive(true);
+        uiBlocker.GetComponent<Image>().DOFade(.7f, durration);
         gameObject.SetActive(true);
         await transform.DOMoveY(0, durration).SetUpdate(true).SetEase(Ease.OutBounce).AsyncWaitForCompletion();
     }
 
     public virtual async Task Close(float durration)
     {
+        uiBlocker.GetComponent<Image>().DOFade(0f, durration);
         await transform.DOMoveY(height, durration).SetUpdate(true).AsyncWaitForCompletion();
         gameObject.SetActive(false);
         uiBlocker.gameObject.SetActive(false);
