9a74c6f201a7bb98bbeb7f8ff5a8666922e081e4
RomanGunger <71374915+RomanGunger@users.noreply.github.com>
2024-05-30 19:25:26 +0300

    fonts update + currency levels UI fix

--- Assets/Sources/Scripts/UI/ScoreUI.cs
+++ Assets/Sources/Scripts/UI/ScoreUI.cs
@@ -1,28 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using TMPro;
 
-[RequireComponent(typeof(UIDocument))]
 public class ScoreUI : MonoBehaviour
 {
-    UIDocument uiDocument;
-    VisualElement rootElement;
-
-    Label textElement;
+    [SerializeField] TextMeshProUGUI text;
 
     private void Start()
     {
         ScoreHandler.ScoreAdded += OnScoreAdded;
-
-        uiDocument = GetComponent<UIDocument>();
-        rootElement = uiDocument.rootVisualElement;
-
-        textElement = rootElement.Q<Label>("score-text");
     }
 
     public void OnScoreAdded(int value)
     {
-        textElement.text = value.ToString();
+        text.text = value.ToString();
     }
 }
